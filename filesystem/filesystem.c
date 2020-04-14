3dc5287853b4c0cecbf6e94d72d7ddad3a694372
alvarogonzavega <alvarogonzavega@gmail.com>
2020-04-14 21:55:11 +0200

    Add files via upload

---
+++ filesystem/filesystem.c
@@ -0,0 +1,186 @@
+
+/*
+ *
+ * Operating System Design / Diseño de Sistemas Operativos
+ * (c) ARCOS.INF.UC3M.ES
+ *
+ * @file 	filesystem.c
+ * @brief 	Implementation of the core file system funcionalities and auxiliary functions.
+ * @date	Last revision 01/04/2020
+ *
+ */
+
+
+#include "filesystem/filesystem.h" // Headers for the core functionality
+#include "filesystem/auxiliary.h"  // Headers for auxiliary functions
+#include "filesystem/metadata.h"   // Type and structure declaration of the file system
+
+char i_map[MAX_N_INODES];
+char b_map[MAX_SIZE_SYS_FILES / BLOCK_SIZE];
+sb sbk[1];
+inode *inodo;
+char *disk = "disk.dat";
+
+/*
+ * @brief 	Generates the proper file system structure in a storage device, as designed by the student.
+ * @return 	0 if success, -1 otherwise.
+ */
+int mkFS(long deviceSize)
+{
+
+	if(deviceSize > MAX_SIZE_SYS_FILES || deviceSize < MIN_SIZE_SYS_FILES) return -1;
+	sbk[0].magic_number = MAGIC_NUMBER;
+	int check = MAX_N_INODES * sizeof(inodo) / BLOCK_SIZE;
+	if(check < 1) sbk[0].num_Blocks_Map_Inodes = 1;
+	else if(check > MAX_N_INODES) return -1;
+	else sbk[0].num_Blocks_Map_Inodes = check;
+	sbk[0].num_Blocks = deviceSize / BLOCK_SIZE;
+	check = sbk[0].num_Blocks * sizeof(int) / BLOCK_SIZE;
+	if(check < 1) sbk[0].num_Blocks_Map_Data = 1;
+	else if(check > MAX_N_INODES) return -1;
+	else sbk[0].num_Blocks_Map_Data = check;
+	sbk[0].num_inodes = MAX_N_INODES;
+	sbk[0].first = 0;
+	sbk[0].size = deviceSize;
+	sbk[0].num_Blocks_Data = sbk[0].num_Blocks - sbk[0].num_Blocks_Map_Data - sbk[0].num_Blocks_Map_Inodes;
+	
+
+}
+
+/*
+ * @brief 	Mounts a file system in the simulated device.
+ * @return 	0 if success, -1 otherwise.
+ */
+int mountFS(void)
+{
+	return -1;
+}
+
+/*
+ * @brief 	Unmounts the file system from the simulated device.
+ * @return 	0 if success, -1 otherwise.
+ */
+int unmountFS(void)
+{
+	return -1;
+}
+
+/*
+ * @brief	Creates a new file, provided it it doesn't exist in the file system.
+ * @return	0 if success, -1 if the file already exists, -2 in case of error.
+ */
+int createFile(char *fileName)
+{
+	return -2;
+}
+
+/*
+ * @brief	Deletes a file, provided it exists in the file system.
+ * @return	0 if success, -1 if the file does not exist, -2 in case of error..
+ */
+int removeFile(char *fileName)
+{
+	return -2;
+}
+
+/*
+ * @brief	Opens an existing file.
+ * @return	The file descriptor if possible, -1 if file does not exist, -2 in case of error..
+ */
+int openFile(char *fileName)
+{
+	return -2;
+}
+
+/*
+ * @brief	Closes a file.
+ * @return	0 if success, -1 otherwise.
+ */
+int closeFile(int fileDescriptor)
+{
+	return -1;
+}
+
+/*
+ * @brief	Reads a number of bytes from a file and stores them in a buffer.
+ * @return	Number of bytes properly read, -1 in case of error.
+ */
+int readFile(int fileDescriptor, void *buffer, int numBytes)
+{
+	return -1;
+}
+
+/*
+ * @brief	Writes a number of bytes from a buffer and into a file.
+ * @return	Number of bytes properly written, -1 in case of error.
+ */
+int writeFile(int fileDescriptor, void *buffer, int numBytes)
+{
+	return -1;
+}
+
+/*
+ * @brief	Modifies the position of the seek pointer of a file.
+ * @return	0 if succes, -1 otherwise.
+ */
+int lseekFile(int fileDescriptor, long offset, int whence)
+{
+	return -1;
+}
+
+/*
+ * @brief	Checks the integrity of the file.
+ * @return	0 if success, -1 if the file is corrupted, -2 in case of error.
+ */
+
+int checkFile (char * fileName)
+{
+    return -2;
+}
+
+/*
+ * @brief	Include integrity on a file.
+ * @return	0 if success, -1 if the file does not exists, -2 in case of error.
+ */
+
+int includeIntegrity (char * fileName)
+{
+    return -2;
+}
+
+/*
+ * @brief	Opens an existing file and checks its integrity
+ * @return	The file descriptor if possible, -1 if file does not exist, -2 if the file is corrupted, -3 in case of error
+ */
+int openFileIntegrity(char *fileName)
+{
+
+    return -2;
+}
+
+/*
+ * @brief	Closes a file and updates its integrity.
+ * @return	0 if success, -1 otherwise.
+ */
+int closeFileIntegrity(int fileDescriptor)
+{
+    return -1;
+}
+
+/*
+ * @brief	Creates a symbolic link to an existing file in the file system.
+ * @return	0 if success, -1 if file does not exist, -2 in case of error.
+ */
+int createLn(char *fileName, char *linkName)
+{
+    return -1;
+}
+
+/*
+ * @brief 	Deletes an existing symbolic link
+ * @return 	0 if the file is correct, -1 if the symbolic link does not exist, -2 in case of error.
+ */
+int removeLn(char *linkName)
+{
+    return -2;
+}
---
+++ filesystem/metadata.h
@@ -0,0 +1,50 @@
+
+/*
+ *
+ * Operating System Design / Diseño de Sistemas Operativos
+ * (c) ARCOS.INF.UC3M.ES
+ *
+ * @file 	metadata.h
+ * @brief 	Definition of the structures and data types of the file system.
+ * @date	Last revision 01/04/2020
+ *
+ */
+
+#define bitmap_getbit(bitmap_, i_) (bitmap_[i_ >> 3] & (1 << (i_ & 0x07)))
+static inline void bitmap_setbit(char *bitmap_, int i_, int val_) {
+  if (val_)
+    bitmap_[(i_ >> 3)] |= (1 << (i_ & 0x07));
+  else
+    bitmap_[(i_ >> 3)] &= ~(1 << (i_ & 0x07));
+}
+
+#define MAGIC_NUMBER 12345
+#define MAX_N_INODES 48
+#define BLOCK_SIZE 2048
+#define MAX_SIZE_FILE 2048
+#define MAX_NAME_LENGTH 32
+#define MIN_SIZE_SYS_FILES 460 * 1024
+#define MAX_SIZE_SYS_FILES 600 * 1024
+
+typedef struct{
+
+  unsigned int magic_number;
+  unsigned int num_Blocks_Map_Inodes;
+  unsigned int num_Blocks_Map_Data;
+  unsigned int num_inodes;
+  unsigned int first;
+  unsigned int size;
+  unsigned int num_Blocks_Data;
+  unsigned int num_Blocks;
+
+}sb;
+
+typedef struct{
+
+  unsigned int type;
+  unsigned int size;
+  unsigned int block;
+  unsigned int state;
+  char name[MAX_NAME_LENGTH];
+
+}inode;
